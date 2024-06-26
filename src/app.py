09fc690c7efaab8a8178d8cda8d92b37727223a5
enzomarx <enzomarxsantos@gmail.com>
2024-06-26 15:09:29 -0300

    geral

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, ttk
 from threading import Thread
 import pandas as pd
 import pyautogui
 import time
+from PIL import ImageTk, Image
 
 pyautogui.PAUSE = 0.5
 
@@ -34,16 +35,22 @@ def start_automation():
 
 def run_automation(df, multiplier):
     start_time = time.time()
+    total_tasks = len(df)
+    completed_tasks = 0
 
     for index, row in df.iterrows():
+        status_label.config(text=f"Processando linha {index+1} de {total_tasks}")
+        progress['value'] = (completed_tasks / total_tasks) * 100
+        app.update_idletasks()
+        
         # Login Dominio Web
         pyautogui.press('win')
         time.sleep(0.5)
-        pyautogui.write('edge', interval=0.25)  
+        pyautogui.write('edge', interval=0.25)
         pyautogui.press('enter')
         time.sleep(2 * multiplier)
 
-        pyautogui.write('https://www.dominioweb.com.br/', interval=0.25)  
+        pyautogui.write('https://www.dominioweb.com.br/', interval=0.25)
         pyautogui.press('enter')
         time.sleep(5 * multiplier)
 
@@ -93,12 +100,17 @@ def run_automation(df, multiplier):
         pyautogui.click(1026, 605)
         pyautogui.click(1324, 343)
         time.sleep(1 * multiplier)
+        
+        completed_tasks += 1
 
     pyautogui.hotkey('alt', 'f4')
 
     end_time = time.time()
     execution_time = end_time - start_time
     print(f"Tempo de execução: {execution_time} segundos")
+    status_label.config(text="Processo concluído")
+    progress['value'] = 100
+    app.update_idletasks()
 
 def estimate_time(df):
     num_entries = len(df)
@@ -113,34 +125,61 @@ def show_help():
                  "3. Clique em 'Iniciar' para começar a automação.")
     messagebox.showinfo("Ajuda", help_text)
 
+def pulse_text():
+    current_color = status_label.cget("foreground")
+    next_color = "red" if current_color == "blue" else "blue"
+    status_label.config(foreground=next_color)
+    app.after(500, pulse_text)
+
+path = r"C:\Users\PC\Downloads\DECS-removebg-preview.png"
+
 app = tk.Tk()
 app.title("Automação DCTF")
-app.geometry('800x400')
+app.geometry('800x800')
+#app.attributes('-fullscreen', True)
 app.configure(bg='#FFECA1')
 
 font_style_title = ('Helvetica', 16, 'bold')
 font_style_label = ('Helvetica', 12)
 font_style_button = ('Helvetica', 12, 'bold')
 
-tk.Label(app, text="Arquivo CSV:", bg='#FFECA1', font=font_style_label).grid(row=0, column=0, pady=20, padx=20, sticky='e')
-cnpj_entry = tk.Entry(app, width=60)
+frame = tk.Frame(app, bg='#FFECA1')
+frame.pack(expand=True)
+
+tk.Label(frame, text="Arquivo CSV:", bg='#FFECA1', font=font_style_label).grid(row=0, column=0, pady=20, padx=20, sticky='e')
+cnpj_entry = tk.Entry(frame, width=60)
 cnpj_entry.grid(row=0, column=1, pady=20, padx=10, sticky='w')
 
-upload_button = tk.Button(app, text="Upload CSV", command=upload_csv, bg='#4169e1', fg='white', font=font_style_button, relief='raised')
+upload_button = tk.Button(frame, text="Upload CSV", command=upload_csv, bg='#4169e1', fg='white', font=font_style_button, relief='raised')
 upload_button.grid(row=1, column=0, pady=10, padx=20)
 
-tk.Label(app, text="Multiplicador de Tempo:", bg='#FFECA1', font=font_style_label).grid(row=2, column=0, pady=20, padx=20, sticky='e')
-sleep_multiplier_entry = tk.Entry(app, width=10)
+tk.Label(frame, text="Multiplicador de Tempo:", bg='#FFECA1', font=font_style_label).grid(row=2, column=0, pady=20, padx=20, sticky='e')
+sleep_multiplier_entry = tk.Entry(frame, width=10)
 sleep_multiplier_entry.grid(row=2, column=1, pady=20, padx=10, sticky='w')
 sleep_multiplier_entry.insert(0, "1.0")  # Valor padrão
 
-start_button = tk.Button(app, text="Iniciar", command=start_automation, bg='#228b22', fg='white', font=font_style_button, relief='raised')
+start_button = tk.Button(frame, text="Iniciar", command=start_automation, bg='#228b22', fg='white', font=font_style_button, relief='raised')
 start_button.grid(row=3, column=1, pady=10, padx=10, sticky='w')
 
-help_button = tk.Button(app, text="Ajuda", command=show_help, bg='#FFA500', fg='white', font=font_style_button, relief='raised')
+help_button = tk.Button(frame, text="Ajuda", command=show_help, bg='#FFA500', fg='white', font=font_style_button, relief='raised')
 help_button.grid(row=3, column=0, pady=10, padx=10, sticky='e')
 
-estimated_time_label = tk.Label(app, text="Estimativa de tempo de execução: N/A", bg='#FFECA1', font=font_style_label)
+estimated_time_label = tk.Label(frame, text="Estimativa de tempo de execução: N/A", bg='#FFECA1', font=font_style_label)
 estimated_time_label.grid(row=4, column=0, columnspan=2, pady=20, padx=20)
+
+status_label = tk.Label(frame, text="Seletor de Projeto:", bg='#FFECA1', font=font_style_label)
+status_label.grid(row=2, column=0, columnspan=4, pady=10, padx=50, sticky= 'e')
+
+status_label = tk.Label(frame, text="Status: Aguardando ação", bg='#FFECA1', font=font_style_label)
+status_label.grid(row=5, column=0, columnspan=2, pady=10, padx=20)
+
+progress = ttk.Progressbar(frame, orient='horizontal', length=400, mode='determinate')
+progress.grid(row=6, column=0, columnspan=2, pady=20, padx=20)
+
+img = ImageTk.PhotoImage(Image.open(path))
+panel = tk.Label(app, image = img)
+panel.pack(side = "bottom", fill = "both", expand = "yes")
+
+pulse_text()
 
 app.mainloop()
