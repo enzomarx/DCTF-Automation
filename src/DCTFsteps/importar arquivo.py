c03e3976ff9cad3153910b9fea993d4ce0fef6cb
enzomarx <enzomarxsantos@gmail.com>
2024-06-27 16:12:41 -0300

    import

---
+++ src/DCTFsteps/importar arquivo.py
@@ -0,0 +1,11 @@
+import time
+import pyautogui 
+
+pyautogui.PAUSE = 0.5
+
+pyautogui.click(29,39)
+pyautogui.click(81,120)
+pyautogui.doubleClick(642,600)
+pyautogui.click(1035,543) 
+#talvez um ctrl a?
+pyautogui.click(1306,533)
